1c51496d4e0ac7444da475077d8c54577a54de30
ajay <ajay.karode1212@gmail.com>
2023-04-22 01:32:34 +0530

    localhost replaced

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,10 @@
-export const api = 'http://localhost:2000/api';
+// export const api = 'http://localhost:2000/api';
+// export const generatePublicUrl = (fileName) => {
+//     return `http://localhost:2000/public/${fileName}`;
+// }
+
+
+export const api = 'https://e-comm-rest-server-backend-gpzb.onrender.com/api';
 export const generatePublicUrl = (fileName) => {
-    return `http://localhost:2000/public/${fileName}`;
+    return `https://e-comm-rest-server-backend-gpzb.onrender.com/public/${fileName}`;
 }
